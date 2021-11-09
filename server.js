bbfc47f892578caa83e79c4e90072ee9eaa94d60
Sabeer Ali <sabeerali099@gmail.com>
2021-11-09 12:05:36 +0530

    node env

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/paper-dashboard-angular'));
+app.use(express.static(__dirname + '/dist'));
+
 app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+'/dist/paper-dashboard-angular/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/index.html'));
 });
+
 app.listen(process.env.PORT || 8080);
